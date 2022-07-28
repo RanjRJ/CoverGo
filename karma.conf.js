c04659b1b68ea8efd732d50df9f1e2db3887776a
Ranjan <ranjan@dataintics.com>
2022-07-28 08:09:34 +0530

    changing the project names to repository name to remove naming conflict

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/assignmentranjan'),
+      dir: require('path').join(__dirname, './coverage/covergo'),
       subdir: '.',
       reporters: [
         { type: 'html' },
